587442ac61870f96a4470d8c103b4f5eaa2d5af3
rexjoseph <josephrex96@gmail.com>
2024-06-06 08:23:34 -0700

    first tests

--- src/P2PSVault.sol
+++ src/P2PSVault.sol
@@ -1,25 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
+import {SafeERC20} from "openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {console} from "forge-std/console.sol";
 
 contract P2PSVault {
+    using SafeERC20 for IERC20;
+    address betToken;
+    
     address public owner;
+    address public feeRecipient;
     address public publisher;
     address public pendingOwner;
     address public pendingPublisher;
     address public betInitiator;
     address public pendingBetInitiator;
 
+    uint256 public fee;
+    uint256 public pendingFee;
+    uint256 public totalFeeEarned;
+
     // EVENTS
-    event ProposedOwner(address);
-    event ProposedPublisher(address);
-    event NewOwner(address);
-    event NewPublisher(address);
-    event ProposedBetInitiator(address);
-    event NewBetInitiator(address);
+    event ProposedOwner(address guy);
+    event ProposedPublisher(address guy);
+    event NewOwner(address guy);
+    event NewPublisher(address guy);
+    event ProposedBetInitiator(address guy);
+    event NewBetInitiator(address guy);
     event BetCreated(uint256, address, uint256, uint256);
-    event BetPlaced(uint256 betId, address indexed user, uint256 amount, string side);
+    event BetPlaced(uint256 betId, address user, uint256 amount, string side);
     event BetResolved(uint256 betId, string outcome);
-    event WinningsClaimed(address indexed user, uint256 amount);
+    event WinningsClaimed(address user, uint256 amount);
+    event BetRefunded(address user, uint256 amount);
+    event FeeSet(uint256 fee);
+    event BetTokenSet(address token);
+    event FeeClaimed(address guy);
 
     // MODIFIERS
     modifier onlyOwner() {
@@ -60,6 +75,8 @@ contract P2PSVault {
         uint256 amount;
         string side;
         uint256 timestamp;
+        string text;
+        uint256 expiry;
     }
 
     Bet[] public bets;
@@ -68,6 +85,7 @@ contract P2PSVault {
     mapping(address => BetPlacement[]) public userBets;
     mapping(uint256 => mapping(address => uint256)) public betAmounts;
     mapping(uint256 => mapping(string => uint256)) public totalBetAmounts;
+    // mapping(uint256 => uint256) public betIdToTotal;
 
     constructor() {
         owner = msg.sender;
@@ -75,7 +93,7 @@ contract P2PSVault {
 
     function createBet(string memory _text, uint256 _duration, address[] memory _eligibleAddresses) external onlyBetInitiator returns (uint256 id) {
         uint256 betId = bets.length;
-        bets.push(Bet(betId, msg.sender, _text, block.timestamp, "", _duration, 0, false, _eligibleAddresses));
+        bets.push(Bet(betId, msg.sender, _text, block.timestamp, "", block.timestamp + _duration, 0, false, _eligibleAddresses));
         betToCreator[msg.sender] = betId;
         emit BetCreated(betId, msg.sender, block.timestamp, _duration);
         return id;
@@ -85,16 +103,25 @@ contract P2PSVault {
         require(betId < bets.length, "Bet does not exist");
         Bet storage bet = bets[betId];
         require(block.timestamp < bet.createdTime + bet.duration, "Bet has expired");
+
         if (bet.eligibleAddresses.length > 0) {
             require(isEligible(bet.eligibleAddresses, msg.sender), "Not eligible to stake on this bet");
         }
         require(keccak256(bytes(side)) == keccak256(bytes("YES")) || keccak256(bytes(side)) == keccak256(bytes("NO")), "Invalid side");
 
-        userBets[msg.sender].push(BetPlacement(betId, amount, side, block.timestamp));
-        betAmounts[betId][msg.sender] += amount;
-        totalBetAmounts[betId][side] += amount;
+        IERC20(betToken).safeTransferFrom(msg.sender, address(this), amount);
 
-        emit BetPlaced(betId, msg.sender, amount, side);
+        // Calculation of fee
+        uint256 feePaid = (amount * fee) / 1000;
+        uint256 netAmount = amount - feePaid;
+
+        userBets[msg.sender].push(BetPlacement(betId, netAmount, side, block.timestamp, bet.text, bet.duration));
+        betAmounts[betId][msg.sender] += netAmount;
+        totalBetAmounts[betId][side] += netAmount;
+        pendingFee += feePaid;
+        totalFeeEarned += feePaid;
+
+        emit BetPlaced(betId, msg.sender, netAmount, side);
     }
 
     function resolveBet(uint256 betId, string memory outcome) external onlyPublisher {
@@ -102,7 +129,7 @@ contract P2PSVault {
         Bet storage bet = bets[betId];
         require(block.timestamp >= bet.createdTime + bet.duration, "Bet duration not ended");
         require(bet.expired == false, "Bet already resolved");
-        require(keccak256(bytes(outcome)) == keccak256(bytes("YES")) || keccak256(bytes(outcome)) == keccak256(bytes("NO")), "Invalid outcome");
+        require(keccak256(bytes(outcome)) == keccak256(bytes("YES")) || keccak256(bytes(outcome)) == keccak256(bytes("NO")) || keccak256(bytes(outcome)) == keccak256(bytes("DRAW")), "Invalid outcome");
 
         bet.outcome = outcome;
         bet.publishTime = block.timestamp;
@@ -111,28 +138,83 @@ contract P2PSVault {
         emit BetResolved(betId, outcome);
     }
 
-    function claimWinnings() external {
-        uint256 totalWinnings = 0;
+    function claimWinning(uint256 _betId) external {
+    require(userBets[msg.sender].length > 0, "No bets");
+    BetPlacement memory betPlacement = userBets[msg.sender][_betId];
+    Bet storage bet = bets[betPlacement.betId];
 
-        for (uint256 i = 0; i < userBets[msg.sender].length; i++) {
-            BetPlacement memory betPlacement = userBets[msg.sender][i];
-            Bet storage bet = bets[betPlacement.betId];
+    require(bet.expired, "Bet not yet resolved");
 
-            if (bet.expired && keccak256(bytes(bet.outcome)) == keccak256(bytes(betPlacement.side))) {
-                uint256 userBetAmount = betAmounts[betPlacement.betId][msg.sender];
-                uint256 winningAmount = totalBetAmounts[betPlacement.betId]["YES"] + totalBetAmounts[betPlacement.betId]["NO"];
-                uint256 userShare = (userBetAmount * 1e18) / totalBetAmounts[betPlacement.betId][bet.outcome];
-                totalWinnings += (userShare * winningAmount) / 1e18;
+    if (keccak256(bytes(bet.outcome)) == keccak256(bytes(betPlacement.side))) {
+        uint256 userBetAmount = betAmounts[betPlacement.betId][msg.sender];
 
-                // prevent double claims
-                betAmounts[betPlacement.betId][msg.sender] = 0;
-            }
-        }
+        // Calculate the total amount bet on the winning side
+        uint256 winningAmount = totalBetAmounts[betPlacement.betId][bet.outcome];
+
+        // Calculate the total amount bet on the losing side
+        string memory losingSide = keccak256(bytes(bet.outcome)) == keccak256(bytes("YES")) ? "NO" : "YES";
+        uint256 losingAmount = totalBetAmounts[betPlacement.betId][losingSide];
+
+        // Calculate the user's percentage share of the total amount bet on the winning side
+        uint256 userSharePercentage = (userBetAmount * 1e18) / winningAmount;
+
+        // Calculate the user's share of the losing side's total stake
+        uint256 userShareOfLosingAmount = (userSharePercentage * losingAmount) / 1e18;
+
+        // Total winnings is the user's initial bet amount plus their share of the losing amount
+        uint256 totalWinnings = userBetAmount + userShareOfLosingAmount;
+
+        // Prevent double claims
+        betAmounts[betPlacement.betId][msg.sender] = 0;
 
         require(totalWinnings > 0, "No winnings to claim");
-        // payable(msg.sender).transfer(totalWinnings);
+
+        // Transfer the winnings to the user
+        IERC20(betToken).safeTransfer(msg.sender, totalWinnings);
+
         emit WinningsClaimed(msg.sender, totalWinnings);
+    } else if (keccak256(bytes(bet.outcome)) == keccak256(bytes("DRAW"))) {
+        uint256 userBetAmount = betAmounts[betPlacement.betId][msg.sender];
+
+        // Prevent double claims
+        betAmounts[betPlacement.betId][msg.sender] = 0;
+
+        require(userBetAmount > 0, "No amount to refund");
+
+        // Refund the stake
+        IERC20(betToken).safeTransfer(msg.sender, userBetAmount);
+
+        emit BetRefunded(msg.sender, userBetAmount);
     }
+}
+
+
+    // function claimWinnings() external {
+    //     uint256 totalWinnings = 0;
+        
+    //     if (userBets[msg.sender].length == 0) {
+    //         revert("No bets");
+    //     }
+
+    //     for (uint256 i = 0; i < userBets[msg.sender].length; i++) {
+    //         BetPlacement memory betPlacement = userBets[msg.sender][i];
+    //         Bet storage bet = bets[betPlacement.betId];
+
+    //         if (bet.expired && keccak256(bytes(bet.outcome)) == keccak256(bytes(betPlacement.side))) {
+    //             uint256 userBetAmount = betAmounts[betPlacement.betId][msg.sender];
+    //             uint256 winningAmount = totalBetAmounts[betPlacement.betId]["YES"] + totalBetAmounts[betPlacement.betId]["NO"];
+    //             uint256 userShare = (userBetAmount * 1e18) / totalBetAmounts[betPlacement.betId][bet.outcome];
+    //             totalWinnings += (userShare * winningAmount) / 1e18;
+
+    //             // prevent double claims
+    //             betAmounts[betPlacement.betId][msg.sender] = 0;
+    //         }
+    //     }
+
+    //     require(totalWinnings > 0, "No winnings to claim");
+    //     // payable(msg.sender).transfer(totalWinnings);
+    //     emit WinningsClaimed(msg.sender, totalWinnings);
+    // }
 
     function isEligible(address[] memory eligibleAddresses, address user) internal pure returns (bool) {
         for (uint256 i = 0; i < eligibleAddresses.length; i++) {
@@ -143,23 +225,46 @@ contract P2PSVault {
         return false;
     }
 
+    function getBet(uint256 betId) external view returns (Bet memory bet) {
+       require(betId < bets.length);
+        bet = bets[betId];
+    }
+
     function getAllBetsCreated() external view returns (Bet[] memory pulledBets) {
-        Bet[] memory initBets = new Bet[](bets.length);
-        uint256 betCounts = 0;
-        uint256 betsLength = bets.length;
+        pulledBets = bets;
+        // Bet[] memory initBets = new Bet[](bets.length);
+        // uint256 betCounts = 0;
+        // uint256 betsLength = bets.length;
 
-        for (uint256 i = 0; i < betsLength; i++) {
-            if (bets[i].expired == false) {
-                initBets[betCounts] = bets[i];
-                betCounts++;
-            }
-        }
+        // for (uint256 i = 0; i < betsLength; i++) {
+        //     if (bets[i].expired == false) {
+        //         initBets[betCounts] = bets[i];
+        //         betCounts++;
+        //     }
+        // }
 
-        pulledBets = new Bet[](betCounts);
-        for (uint256 i = 0; i < betCounts; i++) {
-            pulledBets[i] = initBets[i];
-        }
-        return pulledBets;
+        // pulledBets = new Bet[](betCounts);
+        // for (uint256 i = 0; i < betCounts; i++) {
+        //     pulledBets[i] = initBets[i];
+        // }
+        // return pulledBets;
+    }
+
+    function getSideTotal(uint256 _betId, string memory _side) external view returns (uint256) {
+        return totalBetAmounts[_betId][_side];
+    }
+
+    function getUserSideBet(uint256 _betId, address _user) external view returns (uint256) {
+        return betAmounts[_betId][_user];
+    }
+
+    function getUserBets(address _user) external view returns (BetPlacement[] memory) {
+        return userBets[_user];
+    }
+
+    function setBetToken(address _betToken) public onlyOwner {
+        betToken = _betToken;
+        emit BetTokenSet(_betToken);
     }
 
     function transferOwnership(address _newOwner) public onlyOwner {
@@ -202,5 +307,21 @@ contract P2PSVault {
         betInitiator = pendingBetInitiator;
         pendingBetInitiator = address(0);
         emit NewBetInitiator(msg.sender);
+    }
+
+    function setFee(uint256 _fee) public onlyOwner {
+        fee = _fee;
+        emit FeeSet(_fee);
+    }
+
+    function claimFee() public onlyOwner {
+        require(feeRecipient != address(0), "Error zero address");
+        IERC20(betToken).safeTransfer(feeRecipient, pendingFee);
+        pendingFee = 0;
+        emit FeeClaimed(feeRecipient);
+    }
+
+    function setFeeRecipient(address _guy) public onlyOwner {
+        feeRecipient = _guy;
     }
 }
--- test/P2PSVault.t.sol
+++ test/P2PSVault.t.sol
@@ -3,31 +3,183 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {P2PSVault} from "../src/P2PSVault.sol";
+import {ERC20Token} from "../src/mock/MockERC20.sol";
 
 contract P2PSVaultTest is Test {
     P2PSVault public p2pSVault;
     address owner = makeAddr("owner");
+    address newOwner = makeAddr("newOwner");
     address publisher = makeAddr("publisher");
+    address betInitiator= makeAddr("betInitiator");
+    address feeCollector = makeAddr("feeCollector");
+    ERC20Token USDC;
 
     function setUp() public {
-        vm.prank(owner);
+        vm.startPrank(owner);
+        USDC = new ERC20Token();
+        uint256 amount = 10000 ether;
+        USDC.mint(owner, amount);
         p2pSVault = new P2PSVault();
+        p2pSVault.setBetToken(address(USDC));
+        p2pSVault.setFee(25);
+        vm.stopPrank();
     }
 
     function test_SetPublisher() public {
-        assertEq(p2pSVault.pendingPublisher(), address(0));
-        assertEq(p2pSVault.publisher(), address(0));
+        // assertEq(p2pSVault.pendingPublisher(), address(0));
+        // assertEq(p2pSVault.publisher(), address(0));
 
         vm.prank(owner);
         p2pSVault.setPublisher(publisher);
-        assertEq(p2pSVault.pendingPublisher(), publisher);
+        // assertEq(p2pSVault.pendingPublisher(), publisher);
 
         vm.prank(publisher);
         p2pSVault.acceptPublishingRole();
 
-        assertEq(p2pSVault.pendingPublisher(), address(0));
-        assertEq(p2pSVault.publisher(), publisher);
+        // assertEq(p2pSVault.pendingPublisher(), address(0));
+        // assertEq(p2pSVault.publisher(), publisher);
     }
 
-    function test_TransferOwnership() public {}
+    function test_TransferOwnership() public {
+        // assertEq(p2pSVault.pendingPublisher(), address(0));
+        // assertEq(p2pSVault.owner(), owner);
+
+        vm.prank(owner);
+        p2pSVault.transferOwnership(newOwner);
+        // assertEq(p2pSVault.pendingOwner(), newOwner);
+
+        vm.prank(newOwner);
+        p2pSVault.acceptOwnership();
+
+        // assertEq(p2pSVault.pendingOwner(), address(0));
+        // assertEq(p2pSVault.owner(), newOwner);
+    }
+
+    function test_setBetInitiator() public {
+        // assertEq(p2pSVault.pendingBetInitiator(), address(0));
+        // assertEq(p2pSVault.betInitiator(), address(0));
+
+        vm.prank(owner);
+        p2pSVault.setBetInitiator(betInitiator);
+        // assertEq(p2pSVault.pendingBetInitiator(), betInitiator);
+
+        vm.prank(betInitiator);
+        p2pSVault.acceptBetInitiatorRole();
+
+        // assertEq(p2pSVault.pendingBetInitiator(), address(0));
+        // assertEq(p2pSVault.betInitiator(), betInitiator);
+    }
+
+    function test_setFeeRecipient() public {
+        console.log("Fee collector before set: ", p2pSVault.feeRecipient());
+
+        vm.prank(owner);
+        p2pSVault.setFeeRecipient(feeCollector);
+
+        console.log("Fee collector after set: ", p2pSVault.feeRecipient());
+    }
+
+    function test_createBet() public {
+        test_setBetInitiator();
+        test_SetPublisher();
+        test_setFeeRecipient();
+        
+        vm.prank(betInitiator);
+
+        string memory text = "Trump will win the 2024 US General Elections?";
+        uint256 duration = 259200; // 3 days
+        address[] memory eligibleAddr = new address[](5);
+
+        eligibleAddr[0] = address(111111);
+        eligibleAddr[1] = address(222222); 
+        eligibleAddr[2] = address(333333); 
+        eligibleAddr[3] = address(444444); 
+        eligibleAddr[4] = address(555555); 
+
+        uint256 betId = p2pSVault.createBet(text, duration, eligibleAddr);
+
+        console.log("Created bet with ID: ", betId);
+    }
+
+    function test_placeBet() public {
+        test_createBet();
+
+        vm.startPrank(owner);
+        USDC.transfer(address(111111), 1e18);
+        USDC.transfer(address(222222), 2e18);
+        USDC.transfer(address(333333), 3e18);
+        USDC.transfer(address(444444), 4e18);
+        USDC.transfer(address(555555), 5e18);
+        vm.stopPrank();
+        
+        vm.startPrank(address(111111));
+        USDC.approve(address(p2pSVault), 1e18);
+        p2pSVault.placeBet(0, 1e18, "YES");
+        vm.stopPrank();
+
+        vm.startPrank(address(222222));
+        USDC.approve(address(p2pSVault), 2e18);
+        p2pSVault.placeBet(0, 2e18, "NO");
+        vm.stopPrank();
+
+        vm.startPrank(address(333333));
+        USDC.approve(address(p2pSVault), 3e18);
+        p2pSVault.placeBet(0, 3e18, "YES");
+        vm.stopPrank();
+
+        vm.startPrank(address(444444));
+        USDC.approve(address(p2pSVault), 4e18);
+        p2pSVault.placeBet(0, 4e18, "NO");
+        vm.stopPrank();
+
+        vm.startPrank(address(555555));
+        USDC.approve(address(p2pSVault), 5e18);
+        p2pSVault.placeBet(0, 5e18, "YES");
+        vm.stopPrank();
+
+        uint256 totalYesBets = p2pSVault.totalBetAmounts(0, "YES");
+        uint256 totalNoBets = p2pSVault.totalBetAmounts(0, "NO");
+
+        console.log("TOTAL YES BET: ", totalYesBets);
+        console.log("TOTAL NO BET: ", totalNoBets);
+
+        console.log("CONTRACT BAL AFTER BETS: ", USDC.balanceOf(address(p2pSVault)));
+    }
+
+    function test_resolveBet() public {
+        test_placeBet();
+
+        vm.warp(block.timestamp + 3 days + 1 minutes);
+        
+        vm.prank(publisher);
+        p2pSVault.resolveBet(0, "YES");
+    }
+
+    function test_claimWinning() public {
+        test_resolveBet();
+
+        vm.prank(address(111111));
+        p2pSVault.claimWinning(0);
+
+        vm.prank(address(333333));
+        p2pSVault.claimWinning(0);
+
+        vm.prank(address(555555));
+        p2pSVault.claimWinning(0);
+
+        console.log("Pending Fee: ", p2pSVault.pendingFee());
+        console.log("Total Fee: ", p2pSVault.totalFeeEarned());
+
+        console.log("CONTRACT BAL AFTER WINNING CLAIMS: ", USDC.balanceOf(address(p2pSVault)));
+
+        console.log("USER 1 BAL: ", USDC.balanceOf(address(111111)));
+        console.log("USER 3 BAL: ", USDC.balanceOf(address(333333)));
+        console.log("USER 5 BAL: ", USDC.balanceOf(address(555555)));
+
+        vm.prank(owner);
+        p2pSVault.claimFee();
+
+        console.log("FEE COLLECTOR BAL AFTER FEE CLAIMS: ", USDC.balanceOf(address(feeCollector)));
+        console.log("CONTRACT BAL AFTER FEE CLAIMS: ", USDC.balanceOf(address(p2pSVault)));
+    }
 }
